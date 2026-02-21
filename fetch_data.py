66fccb5d3a2c7bdbef5498f4627592447900a70a
Anvith Bekal <anvithnbekal@gmail.com>
2026-02-21 13:59:15 -0500

    20 racers simulation

--- fetch_data.py
+++ fetch_data.py
@@ -2,30 +2,40 @@ import requests
 import json
 import os
 
-def download_driver_location(session_key, driver_number):
-    print(f"Fetching data for Driver {driver_number}...")
+def download_driver_location(session_key):
+    print(f"Fetching data for Session {session_key}...")
     
     # 1. Ping the OpenF1 API
-    url = f"https://api.openf1.org/v1/location?session_key={session_key}&driver_number={driver_number}"
+    url = f"https://api.openf1.org/v1/drivers?session_key={session_key}"
     response = requests.get(url)
     
-    if response.status_code == 200:
-        data = response.json()
+    if response.status_code != 200:
+        print(f"Failed to get the drivers list: Status: {response.status}")
+    drivers_list = response.json()
         
-        # 2. Ensure the race_data directory exists
-        os.makedirs('race_data', exist_ok=True)
-        
-        # 3. Create a clean filename
-        filename = f"driver_{driver_number}_session_{session_key}.json"
-        filepath = os.path.join('race_data', filename)
-        
-        # 4. Save the data to a local JSON file
-        with open(filepath, 'w') as file:
-            json.dump(data, file)
-            
-        print(f"Success! Saved {len(data)} location points to {filepath}")
-    else:
-        print(f"Failed to fetch data. Error: {response.status_code}")
+    driver_numbers = list(set([driver['driver_number'] for driver in drivers_list]))
+    print('Gate 1')
 
-# Example: Run it for Fernando Alonso (14) at the 2023 Monaco GP (9078)
-download_driver_location(9094, 14)
+    locations = []
+
+    for number in driver_numbers:
+        print(f"Fetching locations for driver {number}")
+        location_url = f"https://api.openf1.org/v1/location?session_key={session_key}&driver_number={number}"
+        
+        locations_info = requests.get(location_url)
+
+        if locations_info.status_code == 200:
+            driver_locations = locations_info.json()
+            locations.extend(driver_locations)
+            print(f"dowloaded {len(driver_locations)} points")
+        else:
+            print(f"Failed to get driver {number} locations. Status code: {locations_info.status_code}")
+    os.makedirs('race_data', exist_ok=True)
+    filepath = os.path.join('race_data', f"session_{session_key}_all.json")
+
+    with open(filepath, 'w') as file:
+        json.dump(locations, file)
+    
+    print(f"\n Finished generating the results of {len(locations)} location points to {filepath}")
+
+download_driver_location(9094)
